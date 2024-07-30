1395dac3d09e9b195e9179287f24165e079310da
paku <p4blo.kuhn@gmail.com>
2024-07-30 00:16:41 -0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.forEach((rule) => {
+        if (rule.use) {
+          rule.use.forEach((u) => {
+            if (u.loader && u.loader.includes('source-map-loader')) {
+              u.exclude = /node_modules/
+            }
+          })
+        }
+      })
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
