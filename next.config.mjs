a75b209f51bee3cf240462e4c5e7e87163ec69b3
paku <p4blo.kuhn@gmail.com>
2024-07-30 00:36:05 -0300

    no next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.module.rules.push({
-        test: /\.map$/,
-        use: 'ignore-loader',
-      })
-    }
-
-    return config
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
